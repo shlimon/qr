66fd169315765f269ec7e6e0a1082502eb53368b
Shakwat Limon <shakwat.au@gmail.com>
2022-07-26 16:05:03 +1000

    updating login

--- login.js
+++ login.js
@@ -1,32 +1,30 @@
+$("#login").click(function () {
+  var email = $("#email").val();
+  let password = $("#inputPassword").val();
 
-
-
-
-$("#login").click(function(){
-
-var email = $("#email").val();
-let password = $("#inputPassword").val();
-
- if( email === "info@decentcare.com.au" && password === "DecentCare2020"){
-   window.open("./staff.html");
- }
- else if( email === "baljeet@decentcare.com.au" && password === "BaljeetTL2022"){
-   window.open("./staff.html");
- }
- else if( email === "priyanka@decentcare.com.au" && password === "PriyankaSC2022"){
-   window.open("./staff.html");
- }
- else if( email === "julia@decentcare.com.au" && password === "JuliaSC2022"){
-   window.open("./staff.html");
- }
- else if( email === "kristine@decentcare.com.au" && password === "KristineSC2022"){
-   window.open("./staff.html");
- }
- else{
-   $(".error").css("display","block");
-
- }
-
-
-
+  if (email === "info@decentcare.com.au" && password === "DecentCare2020") {
+    window.open("./management.html");
+  } else if (
+    email === "baljeet@decentcare.com.au" &&
+    password === "BaljeetTL2022"
+  ) {
+    window.open("./directSupports.html");
+  } else if (
+    email === "priyanka@decentcare.com.au" &&
+    password === "PriyankaSC2022"
+  ) {
+    window.open("./supportCoordination.html");
+  } else if (
+    email === "julia@decentcare.com.au" &&
+    password === "JuliaSC2022"
+  ) {
+    window.open("./supportCoordination.html");
+  } else if (
+    email === "kristine@decentcare.com.au" &&
+    password === "KristineSC2022"
+  ) {
+    window.open("./supportCoordination.html");
+  } else {
+    $(".error").css("display", "block");
+  }
 });
