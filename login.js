a37e17523206ea7a300ad814c6c505b8ab72a67c
Shakwat Limon <shakwat.au@gmail.com>
2022-08-02 16:20:05 +1000

    adding passwod

--- login.js
+++ login.js
@@ -24,6 +24,29 @@ $("#login").click(function () {
     password === "KristineSC2022"
   ) {
     window.open("./supportCoordination.html");
+  } else if (email === "adel@decentcare.com.au" && password === "AdelPG2022") {
+    window.open("./directSupports.html");
+  } else if (email === "liz@decentcare.com.au" && password === "LizPG2022") {
+    window.open("./directSupports.html");
+  } else if (email === "sela@decentcare.com.au" && password === "SelaPG2022") {
+    window.open("./directSupports.html");
+  } else if (
+    email === "arsen@decentcare.com.au" &&
+    password === "ArsenPM2022"
+  ) {
+    window.open("./management.html");
+  } else if (email === "qias@decentcare.com.au" && password === "QaisSC2022") {
+    window.open("./supportCoordination.html");
+  } else if (
+    email === "juanita@decentcare.com.au" &&
+    password === "JuanitaSC2022"
+  ) {
+    window.open("./supportCoordination.html");
+  } else if (
+    email === "baljeet@decentcare.com.au" &&
+    password === "BaljeetPG2022"
+  ) {
+    window.open("./management.html");
   } else {
     $(".error").css("display", "block");
   }
